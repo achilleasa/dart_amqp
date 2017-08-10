d2c5f64bc52efba655dbbda5fc2498b873a47148
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-08-10 07:52:46 +0100

    Switch to travis-ci and codecov.io

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dart_ampq
 
-[![Build Status](https://drone.io/github.com/achilleasa/dart_amqp/status.png)](https://drone.io/github.com/achilleasa/dart_amqp/latest)
-[![Coverage Status](https://coveralls.io/repos/achilleasa/dart_amqp/badge.svg)](https://coveralls.io/r/achilleasa/dart_amqp)
+[![Build Status](https://travis-ci.org/achilleasa/dart_amqp.svg?branch=master)](https://travis-ci.org/achilleasa/dart_amqp)
+[![codecov](https://codecov.io/gh/achilleasa/dart_amqp/branch/master/graph/badge.svg)](https://codecov.io/gh/achilleasa/dart_amqp)
 
 Dart AMQP client implementing protocol version 0.9.1
 
--- test/test_coverage.sh
+++ test/test_coverage.sh
@@ -2,37 +2,14 @@
 
 # Build test coverage reports. To use this script you need to:
 # 1) Install lcov package
-# 2) Clone dart-lang/coverage repo from GH (https://github.com/dart-lang/coverage)
-# 3) Run pub update inside the cloned repo
-#
-# Then invoke this script passing the path to the coverage tools folder
-#
-
-COVERAGE_TOOLS=""
-
-for i in "$@"
-do
-case ${i} in
-    -c=*|--coverage-tools=*)
-    COVERAGE_TOOLS="${i#*=}"
-    shift
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-done
-
-if [ "$COVERAGE_TOOLS" == "" ]; then
-    echo "Please enter path to covert coverage tools folder with the -c or --coverage-tools option"
-    exit 1
-fi
+# 2) pub global activate coverage
+# 3) Add .pub-cache/bin folder to your path
 
 # Cleanup the backgrounded unit test runner on exit if something goes wrong
 trap 'kill $(jobs -p)' EXIT
 
-
 # Cleanup
+mkdir -p coverage
 rm -f coverage/coverage.json
 rm -f coverage/coverage.lcov
 rm -f coverage/filtered.lcov
@@ -42,12 +19,12 @@ dart --checked --enable-vm-service --package-root=../packages/ --pause-isolates-
 
 # Run coverage collection tool
 echo "Waiting for unit tests to run..."
-dart --package-root=${COVERAGE_TOOLS}/packages ${COVERAGE_TOOLS}/bin/collect_coverage.dart --port=8181 -o coverage/coverage.json --wait-paused # --resume-isolates
+collect_coverage --port=8181 -o coverage/coverage.json --wait-paused # --resume-isolates
 echo "Collected coverage data..."
 
 # Convert data to LCOV format
 echo "Converting to LCOV format..."
-dart --package-root=${COVERAGE_TOOLS}/packages ${COVERAGE_TOOLS}/bin/format_coverage.dart --package-root=../  -i coverage/coverage.json -l > coverage/coverage.lcov
+format_coverage --package-root=../  -i coverage/coverage.json -l > coverage/coverage.lcov
 
 # Remove LCOV blocks that do not belong to our project lib/ folder
 echo "Filtering unrelated files from the LCOV data..."
@@ -55,8 +32,12 @@ PROJECT_ROOT=`pwd | sed -e "s/\\/[^\\/]*$//"`
 sed -n '\:^SF.*'"$PROJECT_ROOT"'/lib:,\:end_of_record:p' coverage/coverage.lcov > coverage/filtered.lcov
 
 # Format LCOV data to HTML
-echo "Rendering HTML coverage report to: coverage/html"
-genhtml coverage/filtered.lcov --output-directory coverage/html --ignore-errors source --quiet
+if [ -n "genhtml" ]; then 
+    echo "Rendering HTML coverage report to: coverage/html"
+    genhtml coverage/filtered.lcov --output-directory coverage/html --ignore-errors source --quiet
+    echo "The generated coverage data is available here: "`pwd`"/coverage/html/index.html"
+fi
 
-echo "The generated coverage data is available here: "`pwd`"/coverage/html/index.html"
+rm -f coverage/filtered.lcov
+
 echo
